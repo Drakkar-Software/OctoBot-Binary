df4953cb6c699e890c008a30033dccd3feced34b
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2024-01-18 11:35:46 +0100

    [CI] add pip freeze

--- build_scripts/windows.ps1
+++ build_scripts/windows.ps1
@@ -2,6 +2,7 @@ python -m pip install -U pip setuptools wheel
 python -m pip install -r requirements.txt
 git clone -q $env:OCTOBOT_GH_REPO -b $env:OCTOBOT_DEFAULT_BRANCH
 python -m pip install --prefer-binary -r $env:OCTOBOT_REPOSITORY_DIR/dev_requirements.txt -r $env:OCTOBOT_REPOSITORY_DIR/requirements.txt
+python -m pip freeze
 python scripts/python_file_lister.py bin/octobot_packages_files.txt $env:OCTOBOT_REPOSITORY_DIR
 python scripts/insert_imports.py $env:OCTOBOT_REPOSITORY_DIR/octobot/cli.py
 Copy-Item bin $env:OCTOBOT_REPOSITORY_DIR -recurse
